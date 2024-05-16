551cbad60eef267316cf103b0165f70877511e0e
William Franklin <emailwilliamfranklin@gmail.com>
2024-05-15 18:23:41 -0700

    Added formation testing

--- model.js
+++ model.js
@@ -16,11 +16,18 @@ camera.position.set(250, -250, 250)
 
 //Lighting Setup
 ambientLight('rgb(255,255,255)', 1, scene);
-ThrePointSpotLight('rgb(244,244,239)', 50, 100, -100, 500, scene);
+ThrePointSpotLight('rgb(244,244,239)', 250, 100, -100, 300, scene);
 //hemisphereLight('rgb(255,255,255)', 'rgb(155,155,155)', 1, scene);
 
+let pitchLength = 340
+let pitchWidth = 220
 //Generic Three Objects
-Plane(scene);
+Plane(scene, pitchLength, pitchWidth);
+
+let firstLine = 30
+let midLine = (pitchLength / 2) - 100
+let defLine = (pitchLength / 2) - 60
+let gkLine = (pitchLength / 2) - 20
 
 const homeColor = 'rgb(255,155,155)'
 const homeGKColor = 'rgb(155,55,55)'
@@ -29,75 +36,110 @@ const awayGKColor = 'rgb(55,55,155)'
 
 function fourthreethree(side) {
     if (side === 'home') {
-        Cylinder(scene, -10, 0, homeColor);
-        Cylinder(scene, -20, 80, homeColor);
-        Cylinder(scene, -20, -80, homeColor);
+        Cylinder(scene, -firstLine + 10, 0, homeColor);
+        Cylinder(scene, -firstLine, 80, homeColor);
+        Cylinder(scene, -firstLine, -80, homeColor);
         
-        Cylinder(scene, -80, 0, homeColor);
-        Cylinder(scene, -50, 40, homeColor);
-        Cylinder(scene, -50, -40, homeColor);
+        Cylinder(scene, -midLine - 10, 0, homeColor);
+        Cylinder(scene, -midLine + 10, 40, homeColor);
+        Cylinder(scene, -midLine + 10, -40, homeColor);
         
-        Cylinder(scene, -110, 30, homeColor);
-        Cylinder(scene, -110, -30, homeColor);
-        Cylinder(scene, -110, 80, homeColor);
-        Cylinder(scene, -110, -80, homeColor);
+        Cylinder(scene, -defLine, 30, homeColor);
+        Cylinder(scene, -defLine, -30, homeColor);
+        Cylinder(scene, -defLine + 10, 80, homeColor);
+        Cylinder(scene, -defLine + 10, -80, homeColor);
         
-        Cylinder(scene, -150, 0, homeGKColor);
+        Cylinder(scene, -gkLine, 0, homeGKColor);
     } else if (side === 'away') {
-        Cylinder(scene, 10, 0, awayColor);
-        Cylinder(scene, 20, 80, awayColor);
-        Cylinder(scene, 20, -80, awayColor);
+        Cylinder(scene, firstLine, 0, awayColor);
+        Cylinder(scene, firstLine, 80, awayColor);
+        Cylinder(scene, firstLine, -80, awayColor);
         
-        Cylinder(scene, 80, 0, awayColor);
-        Cylinder(scene, 50, 40, awayColor);
-        Cylinder(scene, 50, -40, awayColor);
+        Cylinder(scene, midLine, 0, awayColor);
+        Cylinder(scene, midLine, 40, awayColor);
+        Cylinder(scene, midLine, -40, awayColor);
         
-        Cylinder(scene, 110, 30, awayColor);
-        Cylinder(scene, 110, -30, awayColor);
-        Cylinder(scene, 110, 80, awayColor);
-        Cylinder(scene, 110, -80, awayColor);
+        Cylinder(scene, defLine, 30, awayColor);
+        Cylinder(scene, defLine, -30, awayColor);
+        Cylinder(scene, defLine, 80, awayColor);
+        Cylinder(scene, defLine, -80, awayColor);
         
-        Cylinder(scene, 150, 0), awayGKColor;
+        Cylinder(scene, gkLine, 0, awayGKColor);
+
+    }
+}
+function threefourthree(side) {
+    if (side === 'home') {
+        Cylinder(scene, -firstLine + 10, 0, homeColor);
+        Cylinder(scene, -firstLine, 60, homeColor);
+        Cylinder(scene, -firstLine, -60, homeColor);
+        
+        Cylinder(scene, -midLine - 10, 30, homeColor);
+        Cylinder(scene, -midLine - 10, -30, homeColor);
+        Cylinder(scene, -midLine + 10, 80, homeColor);
+        Cylinder(scene, -midLine + 10, -80, homeColor);
+        
+        Cylinder(scene, -defLine, 0, homeColor);
+        Cylinder(scene, -defLine + 10, 60, homeColor);
+        Cylinder(scene, -defLine + 10, -60, homeColor);
+        
+        Cylinder(scene, -gkLine, 0, homeGKColor);
+    } else if (side === 'away') {
+        Cylinder(scene, firstLine, 0, awayColor);
+        Cylinder(scene, firstLine, 60, awayColor);
+        Cylinder(scene, firstLine, -60, awayColor);
+        
+        Cylinder(scene, midLine, 30, awayColor);
+        Cylinder(scene, midLine, -30, awayColor);
+        Cylinder(scene, midLine, 80, awayColor);
+        Cylinder(scene, midLine, -80, awayColor);
+        
+        Cylinder(scene, defLine, 0, awayColor);
+        Cylinder(scene, defLine, 60, awayColor);
+        Cylinder(scene, defLine, -60, awayColor);
+
+        
+        Cylinder(scene, gkLine, 0, awayGKColor);
 
     }
 }
 function fourfourtwo(side) {
     if (side === 'home') {
-        Cylinder(scene, -20, 30, homeColor );
-        Cylinder(scene, -20, -30, homeColor);
+        Cylinder(scene, -firstLine, 30, homeColor );
+        Cylinder(scene, -firstLine, -30, homeColor);
         
-        Cylinder(scene, -80, 20, homeColor);
-        Cylinder(scene, -80, -20, homeColor);
-        Cylinder(scene, -50, 40, homeColor);
-        Cylinder(scene, -50, -40, homeColor);
+        Cylinder(scene, -midLine, 20, homeColor);
+        Cylinder(scene, -midLine, -20, homeColor);
+        Cylinder(scene, -midLine, 40, homeColor);
+        Cylinder(scene, -midLine, -40, homeColor);
         
-        Cylinder(scene, -110, 30, homeColor);
-        Cylinder(scene, -110, -30, homeColor);
-        Cylinder(scene, -110, 80, homeColor);
-        Cylinder(scene, -110, -80, homeColor);
+        Cylinder(scene, -defLine, 30, homeColor);
+        Cylinder(scene, -defLine, -30, homeColor);
+        Cylinder(scene, -defLine, 80, homeColor);
+        Cylinder(scene, -defLine, -80, homeColor);
         
-        Cylinder(scene, -150, 0, homeGKColor);
+        Cylinder(scene, -gkLine, 0, homeGKColor);
     } else if (side === 'away') {
-        Cylinder(scene, 20, 30, awayColor);
-        Cylinder(scene, 20, -30, awayColor);
+        Cylinder(scene, firstLine, 30, awayColor);
+        Cylinder(scene, firstLine, -30, awayColor);
         
-        Cylinder(scene, 60, 30, awayColor);
-        Cylinder(scene, 60, -30, awayColor);
-        Cylinder(scene, 60, 80, awayColor);
-        Cylinder(scene, 60, -80, awayColor);
+        Cylinder(scene, midLine, 30, awayColor);
+        Cylinder(scene, midLine, -30, awayColor);
+        Cylinder(scene, midLine, 80, awayColor);
+        Cylinder(scene, midLine, -80, awayColor);
         
-        Cylinder(scene, 110, 30, awayColor);
-        Cylinder(scene, 110, -30, awayColor);
-        Cylinder(scene, 110, 80, awayColor);
-        Cylinder(scene, 110, -80, awayColor);
+        Cylinder(scene, defLine, 30, awayColor);
+        Cylinder(scene, defLine, -30, awayColor);
+        Cylinder(scene, defLine, 80, awayColor);
+        Cylinder(scene, defLine, -80, awayColor);
         
-        Cylinder(scene, 150, 0, awayGKColor);
+        Cylinder(scene, gkLine, 0, awayGKColor);
 
     }
 }
 
 fourthreethree('home');
-fourfourtwo('away');
+threefourthree('away');
 
 //Window Resize
 windowResize(camera, renderer, sceneContainer);
--- src/model/BaseGeometry.js
+++ src/model/BaseGeometry.js
@@ -1,8 +1,8 @@
 //Module Imports
 import * as THREE from 'https://unpkg.com/three@0.164.1/build/three.module.js';
 
-export function Plane(scene) {
-    const planeGeo = new THREE.PlaneGeometry( 338, 220, 32, 32 )
+export function Plane(scene, length, width) {
+    const planeGeo = new THREE.PlaneGeometry( length, width, 32, 32 )
     const planeColor = new THREE.Color('rgb(140, 217, 71)')
     const planeMat = new THREE.MeshStandardMaterial( {color: planeColor} )
     const plane = new THREE.Mesh ( planeGeo, planeMat )
